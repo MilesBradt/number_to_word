42a8037cac2c4773736bcd8b86ffcf62984eee88
Miles Bradt and Robert Bruce <miles@>
2018-11-14 11:34:02 -0800

    add logic for determining teens vs tens

--- lib/number_to_word.rb
+++ lib/number_to_word.rb
@@ -2,54 +2,72 @@ class Number_To_Word
 
   def initialize()
 
-    @single_words = {
-      1 => "one",
-      2 => "two",
-      3 => "three",
-      4 => "four",
-      5 => "five",
-      6 => "six",
-      7 => "seven",
-      8 => "eight",
-      9 => "nine" }
+    @output = ""
+
+    @single_strings = {
+      "1" => "one",
+      "2" => "two",
+      "3" => "three",
+      "4" => "four",
+      "5" => "five",
+      "6" => "six",
+      "7" => "seven",
+      "8" => "eight",
+      "9" => "nine" }
 
     # aka teens
     @fortnite = {
-      11 => "eleven",
-      12 => "twelve",
-      13 => "thirteen",
-      14 => "fourteen",
-      15 => "fifteen",
-      16 => "sixteen",
-      17 => "seventeen",
-      18 => "eighteen",
-      19 => "nineteen" }
+      "11" => "eleven",
+      "12" => "twelve",
+      "13" => "thirteen",
+      "14" => "fourteen",
+      "15" => "fifteen",
+      "16" => "sixteen",
+      "17" => "seventeen",
+      "18" => "eighteen",
+      "19" => "nineteen" }
 
-    @ten_words = {
-      10 => "ten",
-      20 => "twenty",
-      30 => "thirty",
-      40 => "fourty",
-      50 => "fifty",
-      60 => "sixty",
-      70 => "seventy",
-      80 => "eighty",
-      90 => "ninety" }
+    @ten_strings = {
+      "1" => "ten",
+      "2" => "twenty",
+      "3" => "thirty",
+      "4" => "fourty",
+      "5" => "fifty",
+      "6" => "sixty",
+      "7" => "seventy",
+      "8" => "eighty",
+      "9" => "ninety" }
 
   end
 
   def single_digits(number)
-    @single_words.fetch(number)
+    @single_strings.fetch(number.to_s)
   end
 
   def teens(number)
-    @fortnite.fetch(number)
+    @fortnite.fetch(number.to_s)
   end
 
   def tens(number)
-    @ten_words.fetch(number)
+    # this function takes a two-digit number
+    ten_digit = number.to_s.split("")[0]
+    single_digit = number.to_s.split("")[1]
+    if ten_digit != "1"
+      temp = @ten_strings.fetch(ten_digit)
+      @output = temp + " " + self.single_digits(single_digit)
+    else
+      @output = @output + " " + self.teens(ten_digit + single_digit)
+    end
+    @output
   end
 
+  def translate(number)
+    number_split = number.to_s.split("").reverse()
+    if number_split.length == 2
+      self.tens(number)
+    end
+
+  end
 
 # array[i].concat(array[i + 1])
 end
--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -24,7 +24,21 @@ describe('#tens') do
 
   it("translates a 'non-teens' two digit number to the word version") do
     testing = Number_To_Word.new()
-    expect(testing.tens(10)).to(eq("ten"))
+    expect(testing.tens(15)).to(eq(" fifteen"))
+  end
+
+  it("translates a 'non-teens' two digit number to the word version") do
+    testing = Number_To_Word.new()
+    expect(testing.tens(25)).to(eq("twenty five"))
   end
 
 end
+
+# describe('#translate') do
+#
+#   it("turns multi digit number to an array then reverse them") do
+#     testing = Number_To_Word.new()
+#     expect(testing.translate(123)).to(eq(["3", "2", "1"]))
+#   end
+#
+# end
