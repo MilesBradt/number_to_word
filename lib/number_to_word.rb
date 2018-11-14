438b5e409759481d549f60f7bb25b3e1f1c37337
Miles Bradt and Robert Bruce <miles@>
2018-11-14 15:40:42 -0800

    be fundamentally broken, dont use instance vars this way

--- lib/number_to_word.rb
+++ lib/number_to_word.rb
@@ -50,9 +50,9 @@ class Number_To_Word
     temp_array = @output.split("")
     if temp_array[-1] == " "
       temp_array.pop
-      @output = temp_array.join("")
+      return @output = temp_array.join("")
     else
-      @output
+      return @output
     end
   end
 
@@ -88,17 +88,72 @@ class Number_To_Word
 
   def hundreds(number)
     hundreds_digit = number.to_s.split("")[0]
-    @output = @single_strings.fetch(hundreds_digit) + " hundred "
+    @output = @output + @single_strings.fetch(hundreds_digit) + " hundred "
     self.tens(number.to_s.split("").slice(1,2).join("").to_i)
   end
 
-  def translate(number)
-    number_split = number.to_s.split("").reverse()
-    if number_split.length == 2
-      self.tens(number.slice(1,2))
-    end
+  def thousands(number)
 
+    if number.to_s.length === 4
+      thousands_digit = number.to_s.split("")[0]
+      @output = @output + @single_strings.fetch(thousands_digit) + " thousand "
+      self.hundreds(number.to_s.split("").slice(1..3).join("").to_i)
+      @output
+    elsif number.to_s.length === 5
+      ten_thousands_digit = number.to_s.split("")[0..1]
+      rest_of_it = number.to_s.split("")[2..4]
+      @output = @output + tens(ten_thousands_digit.join("")) + " thousand "
+      self.hundreds(rest_of_it.join(""))
+      @output
+    elsif number.to_s.length === 6
+      hundred_thousands_digit = number.to_s.split("")[0..2]
+      rest_of_it = number.to_s.split("")[3..5]
+      @output = @output + hundreds(hundred_thousands_digit.join("")) + " thousand "
+      binding.pry
+      self.hundreds(rest_of_it.join(""))
+      @output
+    end
   end
+
+  def millions(number)
+    if number.to_s.length === 7
+      millions_digit = number.to_s.split("")[0]
+      rest_of_it = number.to_s.split("")[1..6]
+      @output = @output + @single_strings.fetch(millions_digit) + " million "
+      self.thousands(rest_of_it.join("").to_i)
+    elsif number.to_s.length === 8
+      ten_millions_digit = number.to_s.split("")[0..1]
+      rest_of_it = number.to_s.split("")[2..7]
+      @output = @output + tens(ten_millions_digit.join("")) + " million "
+      self.thousands(rest_of_it.join(""))
+    elsif number.to_s.length === 9
+      hundred_millions_digit = number.to_s.split("")[0..2]
+      rest_of_it = number.to_s.split("")[3..8]
+      @output = @output + hundreds(hundred_millions_digit.join("")) + " million "
+      self.thousands(rest_of_it.join(""))
+    end
+  end
+
+  # def any_number(number)
+  #   hundreds = number / 999
+  #   thousands = number / 9999
+  #   ten_thousands = number / 99999
+  #   hundred_thousands = number / 999999
+  #   millions = number / 9999999
+  #   tens_millions = number / 99999999
+  #   hundred_millions = number / 999999999
+  #
+  #   if hundreds <= 1
+  #
+  #
+  #   number_of_digits = number.to_s.length
+  #   number_to_compute = number_of_digits % 3
+  #   number_split = number.to_s.split("").reverse()
+  #   if number_split.length == 2
+  #     self.tens(number.slice(1,2))
+  #   end
+  #
+  # end
 
 # array[i].concat(array[i + 1])
 end
--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -2,64 +2,93 @@ require ('rspec')
 require('number_to_word')
 require ('pry')
 
-describe('#single_digits') do
+# describe('#single_digits') do
+#
+#   it("translates a single digit number to the word version") do
+#     testing = Number_To_Word.new()
+#     expect(testing.single_digits(3)).to(eq("three"))
+#   end
+# end
+#
+# describe('#teens') do
+#
+#   it("translates a 'teens' number to the word version") do
+#     testing = Number_To_Word.new()
+#     expect(testing.teens(13)).to(eq("thirteen"))
+#   end
+# end
+#
+# describe('#tens') do
+#
+#   it("translates a 'non-teens' two digit number to the word version") do
+#     testing = Number_To_Word.new()
+#     expect(testing.tens(12)).to(eq("twelve"))
+#     testing.reset_output
+#     expect(testing.tens(19)).to(eq("nineteen"))
+#     testing.reset_output
+#     expect(testing.tens(15)).to(eq("fifteen"))
+#     testing.reset_output
+#     expect(testing.tens(10)).to(eq("ten"))
+#   end
+#
+#
+#   it("translates a 'non-teens' two digit number to the word version") do
+#     testing = Number_To_Word.new()
+#     expect(testing.tens(25)).to(eq("twenty five"))
+#     testing.reset_output
+#     expect(testing.tens(47)).to(eq("forty seven"))
+#     testing.reset_output
+#     expect(testing.tens(82)).to(eq("eighty two"))
+#     testing.reset_output
+#     expect(testing.tens(59)).to(eq("fifty nine"))
+#     testing.reset_output
+#   end
+# end
+#
+# describe("#hundreds") do
+#
+#   it("uses the hundreds-place of a number to convert the number to words") do
+#     testing = Number_To_Word.new()
+#     expect(testing.hundreds(235)).to(eq("two hundred thirty five"))
+#     testing.reset_output
+#     expect(testing.hundreds(867)).to(eq("eight hundred sixty seven"))
+#     testing.reset_output
+#     expect(testing.hundreds(420)).to(eq("four hundred twenty"))
+#     testing.reset_output
+#     expect(testing.hundreds(510)).to(eq("five hundred ten"))
+#   end
+# end
+#
+# describe("#thousands") do
+#
+#   it("uses the thousands-place of a number to convert the number to words") do
+#     testing = Number_To_Word.new()
+#     expect(testing.thousands(2335)).to(eq("two thousand three hundred thirty five"))
+#     testing.reset_output
+#     expect(testing.thousands(8637)).to(eq("eight thousand six hundred thirty seven"))
+#     testing.reset_output
+#     expect(testing.thousands(4230)).to(eq("four thousand two hundred thirty"))
+#     testing.reset_output
+#     expect(testing.thousands(5110)).to(eq("five thousand one hundred ten"))
+#     testing.reset_output
+#     expect(testing.thousands(22110)).to(eq("twenty two thousand one hundred ten"))
+    # testing.reset_output
+    # expect(testing.thousands(322110)).to(eq("three hundred twenty two thousand one hundred ten"))
+  describe("#millions") do
 
-  it("translates a single digit number to the word version") do
-    testing = Number_To_Word.new()
-    expect(testing.single_digits(3)).to(eq("three"))
+    it("uses the millions place of a n umber to convert the number to words") do
+      testing = Number_To_Word.new
+      testing.reset_output
+      expect(testing.millions(3221410)).to(eq("three million two hundred twenty one thousand four hundred ten"))
+      testing.reset_output
+      expect(testing.millions(32213230)).to(eq("thirty two million two hundered thirteen thousand two hundred thirty"))
+      testing.reset_output
+      expect(testing.millions(322116331)).to(eq("three hundred twenty two million one hundred sixteen thousand three hundred thirty one"))
+
   end
 end
 
-describe('#teens') do
-
-  it("translates a 'teens' number to the word version") do
-    testing = Number_To_Word.new()
-    expect(testing.teens(13)).to(eq("thirteen"))
-  end
-end
-
-describe('#tens') do
-
-  it("translates a 'non-teens' two digit number to the word version") do
-    testing = Number_To_Word.new()
-    expect(testing.tens(12)).to(eq("twelve"))
-    testing.reset_output
-    expect(testing.tens(19)).to(eq("nineteen"))
-    testing.reset_output
-    expect(testing.tens(15)).to(eq("fifteen"))
-    testing.reset_output
-    expect(testing.tens(10)).to(eq("ten"))
-  end
-
-
-  it("translates a 'non-teens' two digit number to the word version") do
-    testing = Number_To_Word.new()
-    expect(testing.tens(25)).to(eq("twenty five"))
-    testing.reset_output
-    expect(testing.tens(47)).to(eq("forty seven"))
-    testing.reset_output
-    expect(testing.tens(82)).to(eq("eighty two"))
-    testing.reset_output
-    expect(testing.tens(59)).to(eq("fifty nine"))
-    testing.reset_output
-  end
-end
-
-describe("#hundreds") do
-
-  it("uses the hundreds-place of a number to convert the number to words") do
-    testing = Number_To_Word.new()
-    expect(testing.hundreds(235)).to(eq("two hundred thirty five"))
-    testing.reset_output
-    expect(testing.hundreds(867)).to(eq("eight hundred sixty seven"))
-    testing.reset_output
-    expect(testing.hundreds(420)).to(eq("four hundred twenty"))
-    testing.reset_output
-    expect(testing.hundreds(510)).to(eq("five hundred ten"))
-  end
-end
-
-# describe('#translate') do
+# describe('#any_number') do
 #
 #   it("turns multi digit number to an array then reverse them") do
 #     testing = Number_To_Word.new()
